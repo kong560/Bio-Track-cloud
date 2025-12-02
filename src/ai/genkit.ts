c33b2d2a27084d154b93b4398398e5dc7e99fd49
rishi shrivastava <rishishrivastava888@gmail.com>
2025-12-02 15:56:37 +0000

    where is .env

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,9 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 const ai = genkit({
-  plugins: [googleAI({apiKey: 'AIzaSyAZE4lNftgJM2Xufp10IcJUz378nEOdJMU'})],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
 });
 
 export {ai};
