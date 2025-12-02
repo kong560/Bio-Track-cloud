9a7ab81abc91fd03fe5eded3257293444eb9b71e
rishi shrivastava <rishishrivastava888@gmail.com>
2025-12-02 15:20:57 +0000

    again same error

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,3 +1,14 @@
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 config();
 
+import {start} from 'genkit';
+import {googleAI} from '@genkit-ai/google-genai';
+import {genkit} from 'genkit';
+
+import './ai-species-recognition';
+
+genkit({
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+});
+
+start();
