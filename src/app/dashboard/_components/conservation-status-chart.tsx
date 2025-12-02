5cd090b5288998791d63b9c5dd3a6c3299134ebe
rishi shrivastava <rishishrivastava888@gmail.com>
2025-12-02 15:31:52 +0000

    Cannot read properties of undefined (reading 'name') solve this error

--- src/app/dashboard/_components/conservation-status-chart.tsx
+++ src/app/dashboard/_components/conservation-status-chart.tsx
@@ -4,17 +4,37 @@ import { Pie, PieChart, ResponsiveContainer, Cell, Tooltip } from 'recharts';
 import { ChartConfig, ChartContainer, ChartTooltipContent } from '@/components/ui/chart';
 
 const data = [
-  { name: 'Least Concern', value: 400, color: '#22c55e' },
-  { name: 'Near Threatened', value: 300, color: '#facc15' },
-  { name: 'Vulnerable', value: 150, color: '#f97316' },
-  { name: 'Endangered', value: 80, color: '#ef4444' },
-  { name: 'Critically Endangered', value: 50, color: '#991b1b' },
+  { name: 'Least Concern', count: 400, color: 'hsl(var(--chart-2))' },
+  { name: 'Near Threatened', count: 300, color: 'hsl(var(--chart-3))' },
+  { name: 'Vulnerable', count: 150, color: 'hsl(var(--chart-4))' },
+  { name: 'Endangered', count: 80, color: '#ef4444' },
+  { name: 'Critically Endangered', count: 50, color: '#991b1b' },
 ];
 
 const chartConfig = {
-    count: {
-      label: "Species"
-    }
+  count: {
+    label: "Species"
+  },
+  "Least Concern": {
+    label: "Least Concern",
+    color: "hsl(var(--chart-2))",
+  },
+  "Near Threatened": {
+    label: "Near Threatened",
+    color: "hsl(var(--chart-3))",
+  },
+  "Vulnerable": {
+    label: "Vulnerable",
+    color: "hsl(var(--chart-4))",
+  },
+  "Endangered": {
+    label: "Endangered",
+    color: "#ef4444",
+  },
+  "Critically Endangered": {
+    label: "Critically Endangered",
+    color: "#991b1b",
+  }
 } satisfies ChartConfig
 
 export default function ConservationStatusChart() {
@@ -24,7 +44,7 @@ export default function ConservationStatusChart() {
         <ChartContainer config={chartConfig}>
             <PieChart>
                 <Tooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-                <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={120} innerRadius={80} labelLine={false} >
+                <Pie data={data} dataKey="count" nameKey="name" cx="50%" cy="50%" outerRadius={120} innerRadius={80} labelLine={false} >
                     {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={entry.color} />
                     ))}
