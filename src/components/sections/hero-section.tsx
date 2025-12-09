c6e498c50c3566fe2eb399fc57cb5a49fbd5d287
kong560 <kongjoseph585@gmail.com>
2025-12-09 06:08:11 +0530

    Import Link component in hero-section

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
+import Link from "next/link";
 
 export default function HeroSection() {
   const heroImage = PlaceHolderImages.find(p => p.id === 'hero-background');
